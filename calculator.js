ac8508b8029da46c928d6eadd8a90b73d9f17c44
CourierID <45730525+CourierID@users.noreply.github.com>
2023-04-04 20:14:54 -0400

    Add operate function

--- calculator.js
+++ calculator.js
@@ -1,3 +1,7 @@
+let numberOne;
+let numberTwo;
+let operator;
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -12,4 +16,24 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
     return num1 / num2;
+}
+
+function operate(numberOne, numberTwo, operator) {
+    switch(operator) {
+        case "+":
+            add(numberOne, numberTwo)
+            break;
+        case "-":
+            subtract(numberOne, numberTwo)
+            break;
+        case "*":
+            multiply(numberOne, numberTwo)
+            break;
+        case "/":
+            divide(numberOne, numberTwo)
+            break;
+        default:
+            console.log("ERROR");
+            break;
+    }
 }
