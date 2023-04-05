3f60ed98be68379e4a0a090f58ef07b3cf2cf305
CourierID <45730525+CourierID@users.noreply.github.com>
2023-04-04 21:39:15 -0400

    Update screen number when button pressed

--- calculator.js
+++ calculator.js
@@ -1,6 +1,7 @@
 let numberOne;
 let numberTwo;
 let operator;
+let displayNumber = 0;
 
 const displayScreen = document.querySelector('.screen');
 
@@ -25,6 +26,7 @@ const eightButton = document.querySelector('#eightButton');
 const nineButton = document.querySelector('#nineButton');
 const zeroButton = document.querySelector('#zeroButton');
 
+//Operator Functions
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -70,8 +72,30 @@ function clear() {
     displayScreen.textContent = "0";
     numberOne = 0;
     numberTwo = 0;
+    displayNumber = "";
 }
 
 clearButton.addEventListener('click', clear);
+
+
+//Update number when number button pressed
+function updateNumber(e) {
+    if (displayNumber == "0") {
+        displayNumber = "";
+    }
+    displayNumber += e.target.textContent;
+    updateDisplay(displayNumber); 
+}
+
+oneButton.addEventListener('click', updateNumber)
+twoButton.addEventListener('click', updateNumber)
+threeButton.addEventListener('click', updateNumber)
+fourButton.addEventListener('click', updateNumber)
+fiveButton.addEventListener('click', updateNumber)
+sixButton.addEventListener('click', updateNumber)
+sevenButton.addEventListener('click', updateNumber)
+eightButton.addEventListener('click', updateNumber)
+nineButton.addEventListener('click', updateNumber)
+zeroButton.addEventListener('click', updateNumber)
 
 
