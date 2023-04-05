df902f9777e72a02e598b98e88873b5d9be7f05e
CourierID <45730525+CourierID@users.noreply.github.com>
2023-04-04 21:03:45 -0400

    Link all the buttons to the JS

--- calculator.js
+++ calculator.js
@@ -2,6 +2,29 @@ let numberOne;
 let numberTwo;
 let operator;
 
+const displayScreen = document.querySelector('.screen');
+
+//Misc Buttons
+const clearButton = document.querySelector('#clearButton');
+
+//Operator Buttons
+const plusButton = document.querySelector('#plusButton');
+const minusButton = document.querySelector('#minusButton');
+const starButton = document.querySelector('#starButton');
+const slashButton = document.querySelector('#slashButton');
+
+//Number Buttons
+const oneButton = document.querySelector('#oneButton');
+const twoButton = document.querySelector('#twoButton');
+const threeButton = document.querySelector('#threeButton');
+const fourButton = document.querySelector('#fourButton');
+const fiveButton = document.querySelector('#fiveButton');
+const sixButton = document.querySelector('#sixButton');
+const sevenButton = document.querySelector('#sevenButton');
+const eightButton = document.querySelector('#eightButton');
+const nineButton = document.querySelector('#nineButton');
+const zeroButton = document.querySelector('#zeroButton');
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -37,3 +60,18 @@ function operate(numberOne, numberTwo, operator) {
             break;
     }
 }
+
+function updateDisplay(newText) {
+    displayScreen.textContent = newText;
+}
+
+//Set screen number to 0 and clear number buffers
+function clear() {
+    displayScreen.textContent = "0";
+    numberOne = 0;
+    numberTwo = 0;
+}
+
+clearButton.addEventListener('click', clear);
+
+
